e509cc7072912eb29c840c2de6d9e86fc772e4ff
Shaharia Azam <mail@shaharia.com>
2025-04-07 02:00:40 +0200

    Remove unused web UI download logic from root command
    The code for downloading the web UI frontend was removed as it is no longer required. This simplifies the root command implementation by eliminating unnecessary dependencies and logic.

--- cmd/root.go
+++ cmd/root.go
@@ -3,9 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/shaharia-lab/echoy/internal/cli"
-	"github.com/shaharia-lab/echoy/internal/filesystem"
 	telemetryEvent "github.com/shaharia-lab/echoy/internal/telemetry"
-	"github.com/shaharia-lab/echoy/internal/webui"
 	"github.com/shaharia-lab/telemetry-collector"
 	"github.com/spf13/cobra"
 )
@@ -35,12 +33,6 @@ func NewRootCmd(container *cli.Container) *cobra.Command {
 					"Root command executed",
 					nil,
 				)
-			}
-
-			nf := webui.NewFrontendReleaseDownloader("latest", container.Paths[filesystem.CacheWebuiBuild])
-			err := nf.DownloadFrontend()
-			if err != nil {
-				return err
 			}
 
 			return nil
